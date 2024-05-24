5196311a51799b94a65fdc59903632efe8068117
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-05-24 20:31:29 +0300

    первый выгруз для GitHub

---
+++ webinar_24-05-2024.py
@@ -0,0 +1,42 @@
+
+https://www.python.org/downloads/
+https://www.jetbrains.com/toolbox-app/
+https://anydesk.com/ru/downloads/windows
+
+pip install numpy
+python -m pip install numpy
+pip install название_библиотеки
+python -m pip install название_библиотеки
+
+numpy
+pandas
+matplotlib
+pillow
+customtkinter
+django
+flask
+Russian Language Pack for Visual Studio Code
+Python
+jupyter
+SQLTools
+SQLite Viewer
+SQl Server Client(mssql)
+
+"""
+Типы данных:
+"""
+# 1. Integer - 1, 2, 3, 4, 5, 10, 20, 30, 40, 50
+# 2. Float - 4.15, 4.167
+# 3. String - строки (char)
+# 4. Bool - True or False (1 or 0)
+
+# IO
+# Input - входные
+# Output - выходные
+
+
+# TODO: Заметки
+## Дата: 19.05.2024
+## 1.
+## 2.
+## 3.
